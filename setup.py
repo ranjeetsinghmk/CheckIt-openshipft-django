de37423ecb54ec79b2390b0c0f139b55f2a88905
Ranjeet Singh <sranjeet748@gmail.com>
2017-04-09 17:46:28 +0530

    New Change

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='checkit api',
       author='Ranjeet Singh',
       author_email='sranjeet748@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.10', 'PyMySQL', 'djangorestframework', 'pygments'],
+      install_requires=['Django>=1.10', 'PyMySQL', 'djangorestframework', 'pygments', 'corsheaders'],
 )
